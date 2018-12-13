9a0bbc14a92d4bcf6f5e534ccc89c4c6b87a6223
Chris Hetzler <chhetzle@microsoft.com>
2018-12-13 13:41:38 -0600

    Again a typo

--- OpenFireWallPort.sh
+++ OpenFireWallPort.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
-/usr/bin/az az login --service-principal -u $APPID -p $APPPASS --tenant microsoft.onmicrosoft.com
-/usr/bin/az az mysql server firewall-rule create --resource-group MdLiveMySQl-RG --server-name mdlivemysql --name AgentRule --start-ip-address $ip --end-ip-address $ip
+az login --service-principal -u $APPID -p $APPPASS --tenant microsoft.onmicrosoft.com
+az mysql server firewall-rule create --resource-group MdLiveMySQl-RG --server-name mdlivemysql --name AgentRule --start-ip-address $ip --end-ip-address $ip
