5be519c930b3757093e65a16d2b5e36f77736504
Chris Hetzler <chhetzle@microsoft.com>
2018-12-13 12:58:12 -0600

    Renamed script and it is working in bash shell, attempting it with secret variables.

--- OpenFireWallPort.sh
+++ OpenFireWallPort.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 az login --service-principal -u $1 -p $2 --tenant microsoft.onmicrosoft.com
 az mysql server firewall-rule create --resource-group MdLiveMySQl-RG --server-name mdlivemysql --name AgentRule --start-ip-address $ip --end-ip-address $ip
