c626e227f965da9e749f07487ad83ce4f475fb9b
Chris Hetzler <chhetzle@microsoft.com>
2018-12-14 11:44:00 -0600

    Echo in script

--- OpenFireWallPort.sh
+++ OpenFireWallPort.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
-az login --service-principal -u $APP_ID -p $APP_PASS --tenant microsoft.onmicrosoft.com
-az mysql server firewall-rule create --resource-group MdLiveMySQl-RG --server-name mdlivemysql --name AgentRule --start-ip-address $ip --end-ip-address $ip
+echo $ip
